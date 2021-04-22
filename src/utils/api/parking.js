69cf1c5775441cecbdde70fd1a6828d1192a5e75
DavidLiang01 <60051944+DavidLiang01@users.noreply.github.com>
2021-04-22 13:28:13 -0400

    search bar

--- src/utils/api/parking.js
+++ src/utils/api/parking.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 const range = (start, stop, step) =>
   Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + i * step);
 const getParkings = async () => {
+  //const dataRange = range(0, 18400, 100); retrieve ALL parkings
   const dataRange = range(0, 300, 100);
   const parkings = dataRange.map(async (idx) => {
     const options = {
@@ -19,9 +20,7 @@ const getParkings = async () => {
   });
 
   const res = await Promise.all(parkings);
-  const val = res.flat();
-  console.log(val);
-  return val;
+  return res.flat();
 };
 
 export { getParkings };
